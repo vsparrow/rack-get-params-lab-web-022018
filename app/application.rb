78252487751b2e10c24cd7d525bae41ae21b1bb7
vsparrow <khan@vsparrow.com>
2018-03-11 10:02:17 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,8 @@
 class Application
 
+
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []  # Create a new class array called @@cart to hold any items in your cart
 
   def call(env)
     resp = Rack::Response.new
@@ -13,12 +15,29 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)      # Create a new route called /cart to show the items in your cart
+      @@cart.each {|item| resp.write "#{item}\n"} if @@cart.size > 0
+      resp.write "Your cart is empty" if @@cart.size == 0
+    elsif req.path.match(/add/)
+      # Create a new route called /add that takes in a GET param with the key item.
+      add_item = req.params["item"]
+      # STDERR.puts "********#{add_item}||#{req.params}"
+      #   This should check to see if that item is in @@items and then add it to the cart if it is.
+      if @@items.include?(add_item)
+        @@cart << add_item
+        # STDERR.puts "GOT HERE TO ADD ITEM::Added #{add_item}"
+        resp.write "added #{add_item}"
+      #   Otherwise give an error
+      else#else inner
+        # resp.write "Error item not found "
+        resp.write "We don't have that item"
+      end#if inner
     else
       resp.write "Path Not Found"
     end
 
     resp.finish
-  end
+  end#class
 
   def handle_search(search_term)
     if @@items.include?(search_term)
